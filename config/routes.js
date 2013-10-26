4bb61048b3d1978c17a41156cff4afb9cbbaf066
Ryan Roberts <ryansroberts@gmail.com>
2013-10-26 16:37:01 +0100

    Broken

--- config/routes.js
+++ config/routes.js
@@ -4,4 +4,8 @@ module.exports = function(app){
 	var home = require('../app/controllers/home');
 	app.get('/', home.index);
 
+  var user = require('../app/controllers/user');
+  app.post('/user',user.create);
+  app.get('/user:id',user.id);
+
 };
