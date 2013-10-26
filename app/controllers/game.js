d4365e69a034b5c8bce542708e442cadf3c6eae9
Ryan Roberts <ryansroberts@gmail.com>
2013-10-27 00:34:41 +0100

    Joining / starting games

--- app/controllers/game.js
+++ app/controllers/game.js
@@ -10,18 +10,9 @@ exports.open = function(req, res) {
 };
 
 exports.join = function(req, res) {
-    console.log(req.params.id);
     Game.findById(req.params.id, function(error, game) {
-        request("http://namey.muffinlabs.com/name.json?count=1&with_surname=true", function(error, response, body) {
-            if (_.find(game.players, function(p) {
-                return p.user == req.params.id;
-            })) return;
-            game.players.push({
-                user: req.body.id,
-                pseudonym: JSON.parse(body)[0]
-            });
-            game.save();
-            res.send(game);
+        game.addPlayer(req.params.id, function() {
+            res.json(game);
         });
     });
 };
