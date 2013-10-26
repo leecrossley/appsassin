c52197170bcee7af3a5055e6682b8cd797681ed1
edhiley <edhiley@gmail.com>
2013-10-26 17:41:32 +0100

    broken routes

--- config/routes.js
+++ config/routes.js
@@ -4,4 +4,10 @@ module.exports = function(app){
 	var home = require('../app/controllers/home');
 	app.get('/', home.index);
 
+	var pictures = require('../app/controllers/pictures');
+	app.post('/pictures/add', pictures.add);
+	app.get('/pictures', pictures.index);
+	app.get('/pictures/:pictureId', pictures.show);
+
+
 };
